d22272b761c3a283cccac345f714f436016d89f6
Jamie <jamiecsunshine@gmail.com>
2019-04-18 17:45:11 +1200

    added gui for file selection

--- MindaCSV.py
+++ MindaCSV.py
@@ -1,10 +1,16 @@
 import csv
+from tkinter import *
+from tkinter import filedialog
+
+set_date = ''
 
 def dateChanger(date):
+    global set_date
     dateList = {"Jan": "01", "Feb": "02", "Mar": "03", "Apr": "04", "May": "05", "Jun": "06", "Jul": "07",
                 "Aug": "08", "Sep": "09", "Oct": "10", "Nov": "11", "Dec": "12"}
     new = date.split("-")
     final = new[0] + "/" + dateList[new[1]] + "/" + new[2]
+    set_date = new[0] + '-' + dateList[new[1]] + '-' + new [2]
     return final
 
 def convertCSV(file):
@@ -22,15 +28,16 @@ def convertCSV(file):
                 tempList.append(row[2][1:])
                 tempList.append(dateChanger(row[7][1:]))
                 csvData.append(tempList)
-    new_file_name = "Minda Uploads " + ""
 
-    with open('persons.csv', 'w') as csvfile:
+    new_file_name = "Minda Uploads " + set_date + '.csv'
+
+    with open(new_file_name, 'w') as csvfile:
         filewriter = csv.writer(csvfile, delimiter=',',
                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
         for i in csvData:
             filewriter.writerow(i)
 
 
-
-filename = 'FeedWedgePastureCover-15-Apr-2019.csv'
-convertCSV(filename)
+root = Tk()
+root.fileName = filedialog.askopenfilename()
+convertCSV(root.fileName)
